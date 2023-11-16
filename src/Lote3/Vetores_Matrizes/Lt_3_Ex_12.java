47f04823fc92eda1d2e46d24527cef254f5526e2
Thiago Antenor <thiagoantenor31@gmail.com>
2023-11-16 17:37:53 -0300

    Commit update - Envio do ultimo exercício

--- src/Lote3/Vetores_Matrizes/Lt_3_Ex_12.java
+++ src/Lote3/Vetores_Matrizes/Lt_3_Ex_12.java
@@ -4,29 +4,84 @@ import javax.swing.JOptionPane;
 public class Lt_3_Ex_12 {
   /*
    * 12. Carregar códigos das peças em um tabuleiro de xadrez, onde:
-   * Codigo:|1 |2 |3 |4 |5 |6 |7
+   * Codigo:|1  |2    |3    |4      |5     |6  |7
    * Peça: |Peão|Torre|Bispo|Cavalo|Rainha|Rei|Vazio
    * Calcular e mostrar a soma das peças do tabuleiro
    * Não pode utilizar estrutura de decisão e escolha Caso na contagem das peças
    */
   public static void main(String args[]) {
     int mat[][] = new int[8][8];
-    //ProcedureCarregaTabuleiro(mat);
+    ProcedureCarregaTabuleiro(mat);
+    JOptionPane.showMessageDialog(null, "A soma das peças do tabuleiro foi: " + FunctionSomaTabuleiro(mat));
   }
 
   // Carregar peças na mesma posição que ficariam em um tabuleiro de xadrez
-  /*
+  
   static void ProcedureCarregaTabuleiro(int m[][]) {
     for (int i = 0; i < m.length; i++) {
       for (int j = 0; j < m.length; j++) {
-        m[i][j] = Integer.parseInt(JOptionPane.showInputDialog(
-            "Informe a peça de tabuleiro que deseja colocar, usando:\n1 = Peão, 2 = Torre, 3 = Bispo, 4 = Cavalo, 5 = Rainha, 6 = Rei, 7 = Vazio"));
+        switch (j) {
+          //Torre
+          case 0:
+          case 7:
+            if((i == 0 || i == 7)){
+              m[i][j] = 2;
+            } else if(i == 1 || i == 6){
+              m[i][j] = 1;
+            } else if(i == 2 || i == 3 || i == 4 || i == 5){
+              m[i][j] = 7;
+            }
+
+            break;
+        //Cavalo
+        case 1:
+        case 6:
+            if((i == 0 || i == 7)){
+              m[i][j] = 4;
+            }else if(i == 1 || i == 6){
+              m[i][j] = 1;
+            } else if(i == 2 || i == 3 || i == 4 || i == 5){
+              m[i][j] = 7;
+            }
+            break;
+        //Bispo
+        case 2:
+        case 5:
+            if((i == 0 || i == 7)){
+              m[i][j] = 3;
+            }else if(i == 1 || i == 6){
+              m[i][j] = 1;
+            } else if(i == 2 || i == 3 || i == 4 || i == 5){
+              m[i][j] = 7;
+            }
+        break;
+      //Rei
+        case 3:
+            if((i == 0 || i == 7)){
+              m[i][j] = 6;
+            }else if(i == 1 || i == 6){
+              m[i][j] = 1;
+            } else if(i == 2 || i == 3 || i == 4 || i == 5){
+              m[i][j] = 7;
+            }
+        break;
+      //Rainha
+        case 4:
+            if((i == 0 || i == 7)){
+              m[i][j] = 5;
+            }else if(i == 1 || i == 6){
+              m[i][j] = 1;
+            } else if(i == 2 || i == 3 || i == 4 || i == 5){
+              m[i][j] = 7;
+            }
+        break;
+        }
       }
     }
   }
-  */
 
-  /* static int FunctionSomaTabuleiro(int m[][]) {
+
+static int FunctionSomaTabuleiro(int m[][]) {
     int som = 0;
     for(int i = 0; i < m.length; i++){
       for(int j = 0; j < m.length; j++){
@@ -34,7 +89,6 @@ public class Lt_3_Ex_12 {
       }
     }
     return som;
-  }
-  */
+}
 
 }
